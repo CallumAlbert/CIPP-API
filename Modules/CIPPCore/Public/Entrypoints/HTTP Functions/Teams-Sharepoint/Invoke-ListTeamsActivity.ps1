5ae86f18b6a9302373440a1291b046a381c06efd
Callum Albert <93670024+CallumAlbert@users.noreply.github.com>
2025-11-25 14:17:19 +0000

    revert after update

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListTeamsActivity.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListTeamsActivity.ps1
@@ -1,22 +1,34 @@
+using namespace System.Net
+
 Function Invoke-ListTeamsActivity {
     <#
     .FUNCTIONALITY
-        Entrypoint
-    .ROLE
-        Teams.Activity.Read
+    Entrypoint
     #>
     [CmdletBinding()]
     param($Request, $TriggerMetadata)
+
+        
+    # Write to the Azure Functions log stream.
+    Write-Host 'PowerShell HTTP trigger function processed a request.'
+
     # Interact with query parameters or the body of the request.
-    $TenantFilter = $Request.Query.tenantFilter
-    $type = $request.Query.Type
-    $GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/get$($type)Detail(period='D30')" -tenantid $TenantFilter | ConvertFrom-Csv | Select-Object @{ Name = 'UPN'; Expression = { $_.'User Principal Name' } },
+    $TenantFilter = $Request.Query.TenantFilter
+    $type = $request.query.Type
+    $GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/get$($type)Detail(period='D7')" -tenantid $TenantFilter | ConvertFrom-Csv | Select-Object @{ Name = 'UPN'; Expression = { $_.'User Principal Name' } },
     @{ Name = 'LastActive'; Expression = { $_.'Last Activity Date' } },
     @{ Name = 'TeamsChat'; Expression = { $_.'Team Chat Message Count' } },
     @{ Name = 'CallCount'; Expression = { $_.'Call Count' } },
-    @{ Name = 'MeetingCount'; Expression = { $_.'Meeting Count' } }
+    @{ Name = 'MeetingCount'; Expression = { $_.'Meeting Count' } },
+    @{ Name = 'AudioCallDuration'; Expression = { $_.'Audio Duration' } },
+    @{ Name = 'VideoCallDuration'; Expression = { $_.'Video Duration' } },
+    @{ Name = 'ScreenShareDuration'; Expression = { $_.'Screen Share Duration' } },
+    @{ Name = 'PrivateChat'; Expression = { $_.'Private Chat Message Count' } }
+                         
+                        
 
-    return ([HttpResponseContext]@{
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
             StatusCode = [HttpStatusCode]::OK
             Body       = @($GraphRequest)
         })
