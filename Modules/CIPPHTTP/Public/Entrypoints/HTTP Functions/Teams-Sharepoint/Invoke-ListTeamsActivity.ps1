bb894e02647194acfd331647bdf14f1c080dc3d6
Callum Albert <93670024+CallumAlbert@users.noreply.github.com>
2026-06-08 15:50:16 +0100

    reverted changes after update

--- Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListTeamsActivity.ps1
+++ Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListTeamsActivity.ps1
@@ -31,8 +31,12 @@ Function Invoke-ListTeamsActivity {
     $GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/get$($type)Detail(period='D30')" -tenantid $TenantFilter | ConvertFrom-Csv | Select-Object @{ Name = 'UPN'; Expression = { $_.'User Principal Name' } },
     @{ Name = 'LastActive'; Expression = { $_.'Last Activity Date' } },
     @{ Name = 'TeamsChat'; Expression = { $_.'Team Chat Message Count' } },
+    @{ Name = 'PrivateChat'; Expression = { $_.'Private Chat Message Count' } },
     @{ Name = 'CallCount'; Expression = { $_.'Call Count' } },
-    @{ Name = 'MeetingCount'; Expression = { $_.'Meeting Count' } }
+    @{ Name = 'MeetingCount'; Expression = { $_.'Meeting Count' } },
+    @{ Name = 'AudioDuration'; Expression = { $_.'Audio Duration' } },
+    @{ Name = 'VideoDuration'; Expression = { $_.'Video Duration' } },
+    @{ Name = 'ScreenShareDuration'; Expression = { $_.'Screen Share Duration' } }
 
     return ([HttpResponseContext]@{
             StatusCode = [HttpStatusCode]::OK
